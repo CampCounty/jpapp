9a62bdc74c1b31d143b9dd80aee087c0ce7e1062
CampCounty <camp-county@gmx.de>
2024-04-27 07:49:50 +0200

    update

--- lib/presentation/splash_screen.dart
+++ lib/presentation/splash_screen.dart
@@ -22,14 +22,51 @@ class SplashScreen extends StatelessWidget {
 
           // Overlay image
           Positioned(
-            top: 80,
-            left: 300,
-            child: Image.asset('assets/images/grafiken/chick cupcakes_3D.png'),
+            top: 130,
+            left: 100,
+            child: Container(
+              child: Image.asset(
+                'assets/images/grafiken/chick cupcakes_3D.png',
+                fit: BoxFit.cover,
+              ),
+              height: 400,
+              width: 400,
+            ),
           ),
           Positioned(
             top: 400,
-            left: 200,
+            left: 30,
             child: Image.asset('assets/images/details/T2.png'),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 100,
+            child: Container(
+              width: 230, // Fixed width
+              height: 150, // Fixed height
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), // Rounded corners
+                color: Color.fromARGB(
+                    163, 244, 244, 244), // Background color with transparency
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26, // Shadow color
+                    blurRadius: 10.0, // Blur radius
+                    offset: Offset(0.0, 10.0), // Shadow offset
+                    spreadRadius: 5.0, // Spread radius
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'Text hier',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+            ),
           ),
         ],
       ),
